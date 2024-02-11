07affb060f794cef6df278586796156871797abe
tommy-manjaro <153081087+tandc181123@users.noreply.github.com>
2024-02-11 19:50:41 +0800

    chore: first green test

---
+++ py/test_money.py
@@ -0,0 +1,16 @@
+import unittest
+
+class TestMoney(unittest.TestCase):
+    def testMultiplication(self):
+        fiver = Dollar(5)
+        tenner = fiver.times(2)
+        self.assertEqual(10, tenner.amount)
+
+class Dollar:
+    def __init__(self, amount):
+        self.amount = amount
+    def times(self, multiplier):
+        return Dollar(self.amount * multiplier)
+
+if __name__ == '__main__':
+    unittest.main()
